08e911be9ed1768c2ce07aa72b48a3e81b9f373d
diogo <eltigredc@gmail.com>
2022-01-24 16:38:55 +0100

    ooops i forgot some stuff (mainly conditions and functions)

---
+++ condition.js
@@ -0,0 +1,11 @@
+let condition = "yes you can"
+let secondCondition = "ok"
+let thirdCondition = "of course"
+
+if(condition === "yes you can"){
+	console.log("hey im here")
+}else if(secondCondition === "ok"){
+	console.log("im in the else if")
+}else{
+	console.log("no im here")
+}
